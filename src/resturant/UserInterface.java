7035aefc9364cb35753f934d97c9d04a1ca095b5
Aviel Cohen <acohen41@students.towson.edu>
2019-04-14 12:09:45 -0400

    refactored user interface

--- src/resturant/UserInterface.java
+++ src/resturant/UserInterface.java
@@ -6,7 +6,7 @@ public class UserInterface {
 
 	public static void main(String[] args) {
 		
-		Menu menu = getMenu();
+		Menu menu = createMenu();
 		
 		Aggregator agg = new Aggregator(menu, new Orders());
 		
@@ -14,52 +14,64 @@ public class UserInterface {
 		
 		SystemInterface.init(inv);
 		
-		System.out.println("The Menu at Five Guys is:");
+		System.out.println("The Menu at the Resturnat is:");
 		
 		String [] items = SystemInterface.getMenu();
 		
-		for(int i = 0; i < items.length; i++) {
-			
-			System.out.println(items[i]);
-		}
+		printItems(items);
 		
 		Scanner input = new Scanner(System.in);
-		int orderNum = 0;
 		
+		createOrder(menu, input);
+		
+		System.out.println("Your tab is: ");
+		
+		String [] tab = SystemInterface.getTab();
+		
+		printItems(tab);
+		
+
+	}
+
+	/**
+	 * @param menu
+	 * @param input
+	 */
+	public static void createOrder(Menu menu, Scanner input) {
+		int orderNum;
 		do {
 		System.out.println("Please Enter the item number you wish to order: ");
 		orderNum = input.nextInt();
-		
 		
 		boolean placed = SystemInterface.placeOrder(orderNum);
 		
 		if(placed)
 			System.out.println("Your order has been placed");
 		
-		}while(orderNum >0 && orderNum < 4);
-		
-		System.out.println("Your tab is: ");
-		
-		
-		
-		
-		String [] tab = SystemInterface.getTab();
-		
-		for(int i = 0; i < tab.length; i++) {
-			
-			System.out.println(tab[i]);
-		}
-		
+		} while(orderNum > 0 && orderNum <= menu.menuSize());
+	}
 
+	/**
+	 * @param items
+	 */
+	public static void printItems(String[] items) {
+		for(int i = 0; i < items.length; i++) {
+			
+			System.out.println(items[i]);
+		}
 	}
 	
-	public static Menu getMenu () 
+	public static Menu createMenu () 
 	{
 		Menu newMenu = new Menu();
 		
 		newMenu.addItem(new MenuItem(1, "burger", 7.50));
 		newMenu.addItem(new MenuItem(2, "fries", 3.50));
 		newMenu.addItem(new MenuItem(3, "shake", 1.50));
+		
+		newMenu.addItem(new MenuItem(4, "cookie", 2.50));
+		newMenu.addItem(new MenuItem(5, "pizza", 3.50));
+		newMenu.addItem(new MenuItem(6, "soda", 1.50));
 		
 		return newMenu;
 	}
